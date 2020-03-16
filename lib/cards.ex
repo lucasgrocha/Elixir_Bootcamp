78179e6e1bdf7ac37d8f82ec678bfaba47e75e6f
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-03-15 23:50:59 -0300

    fix initial breaking tests

--- lib/cards.ex
+++ lib/cards.ex
@@ -24,15 +24,15 @@ defmodule Cards do
   end
 
   @doc """
-    Divides a deck into a hend and the remainder of the deck.
+    Divides a deck into a hand and the remainder of the deck.
     The `hand_size` argument indicates how many cards should
     be in the hand.
 
   ## Examples
 
       iex> deck = Cards.create_deck
-      iex> {hand deck} = Cards.deal(deck, 1)
-      ["Ace of Spades"]
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> ["Ace of Spades"]
 
   """
   def deal(deck, hand_size) do
